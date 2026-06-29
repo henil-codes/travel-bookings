b93a726dc44230bb9bc134c620c015e98ec68e27
HENILKUMAR PRAFULCHANDRA PATEL <85500288+henil-codes@users.noreply.github.com>
2026-06-29 00:53:15 -0400

    Home Page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
-import { ProctedRoute } from './core/ProctedRoute';
+import { ProctedRoute } from './core/ProctectedRoute';
+import { CustomerLayout } from './layouts/CustomerLayout';
+import { LoginPage } from './pages/customer/LoginPage';
+import { RegisterPage } from './pages/customer/Registerpage';
+import { ForgotPasswordPage } from './pages/customer/ForgotPasswordPage';
+import { ResetPasswordPage } from './pages/customer/ResetPasswordPage';
+
 
 function App() {
   return (
