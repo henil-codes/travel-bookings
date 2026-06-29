a8be29ebfab9ebe18445d31be6781f3d234adbba
HENILKUMAR PRAFULCHANDRA PATEL <85500288+henil-codes@users.noreply.github.com>
2026-06-29 01:19:10 -0400

    comment code for successfull project build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
-import { ProctedRoute } from './core/ProctectedRoute';
-import { CustomerLayout } from './layouts/CustomerLayout';
-import { LoginPage } from './pages/customer/LoginPage';
-import { RegisterPage } from './pages/customer/Registerpage';
-import { ForgotPasswordPage } from './pages/customer/ForgotPasswordPage';
-import { ResetPasswordPage } from './pages/customer/ResetPasswordPage';
+// import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+// import { ProctedRoute } from './core/ProctectedRoute';
+// import { CustomerLayout } from './layouts/CustomerLayout';
+// import { LoginPage } from './pages/customer/LoginPage';
+// import { RegisterPage } from './pages/customer/Registerpage';
+// import { ForgotPasswordPage } from './pages/customer/ForgotPasswordPage';
+// import { ResetPasswordPage } from './pages/customer/ResetPasswordPage';
 
 
 function App() {
